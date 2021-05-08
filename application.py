88841e561a84e5783750a6a372c05faacb3ca113
jvram-619 <72521664+jvram-619@users.noreply.github.com>
2021-05-08 22:56:06 +0400

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print("Handling request to home page.")
+    return "Hello All, This is Venkat. Azure Capstone Project 7"
